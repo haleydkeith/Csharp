e0079d925125568f2c5e9029c3283c72e003db0b
haleydkeith <haleydkeith0905@gmail.com>
2020-08-18 10:03:04 -0500

    looking sharp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c__01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
